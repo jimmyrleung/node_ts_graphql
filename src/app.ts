2ab2c6022ae817caae3882b283cf914d46ca8ca8
Jimmy Leung <jimmy.rl19@hotmail.com>
2018-12-13 20:39:10 -0200

    express configuration

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("Hello world");
+import * as Express from 'express';
+
+class App {
+    public express: Express.Application;
+
+    constructor() {
+        // Returns an express application
+        this.express = Express();
+        this.middleware();
+    }
+
+    private middleware(): void {
+        this.express
+            .use("/hello", (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+                res.send("Hello world");
+            });
+    }
+}
+
+export default new App().express;
